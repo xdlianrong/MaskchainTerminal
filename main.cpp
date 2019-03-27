0c3df68611b9615f0b59b789f879cbf17c348d23
peichengxuan <407949724@qq.com>
2019-03-27 22:07:29 +0800

    small changes in include order

--- main.cpp
+++ main.cpp
@@ -4,8 +4,9 @@
 #include <boost/asio.hpp>
 #include <boost/thread/thread.hpp>
 
-#include <signal.h>
 #include "./term.h"
+
+#include <signal.h>
 #include <iostream>
 #include <string>
 #include <stdlib.h>
@@ -88,7 +89,7 @@ class remoteEndpoint{
             string tmp=rep.substr(idx+1, rep.length()-1);
             this->rmPort=boost::lexical_cast<int>(tmp);
         }
-
+  
         ~remoteEndpoint(){}
 
         int traverser(){
